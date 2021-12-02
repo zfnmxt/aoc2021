abb2351eafad26d05e4c37ff75672be664367b44
zfnmxt <zfnmxt@zfnmxt.com>
2021-12-02 15:22:33 +0100

    Day 2.

---
+++ day2/dive.hs
@@ -0,0 +1,37 @@
+module Dive where
+
+import Data.Char (isSpace)
+
+type Position = (Int, Int)
+
+type State = (Position, Int)
+
+data Command = Forward Int | Down Int | Up Int
+
+parse :: String -> Command
+parse s =
+  case com of
+    "forward" -> Forward n
+    "down" -> Down n
+    "up" -> Up n
+  where
+    (com, rest) = break isSpace
+    n = read rest
+
+move :: Position -> Command -> Position
+move (x, y) (Forward dx) = (x + dx, y)
+move (x, y) (Down dy) = (x, y + dy)
+move (x, y) (Up dy) = (x, y - dy)
+
+move2 :: State -> Command -> State
+move2 ((x, y), a) (Forward dx) = ((x + dx, y + a * dx), a)
+move2 ((x, y), a) (Down da) = ((x, y), a + da)
+move2 ((x, y), a) (Up da) = ((x, y), a - da)
+
+main :: IO ()
+main = do
+  cmds <- map parse . lines <$> getContents
+  let (x1, y1) = foldl move (0, 0) cmds
+  putStrLn $ "part1 (x * y): " <> show (x1 * y1)
+  let ((x2, y2), _) = foldl move2 ((0, 0), 0) cmds
+  putStrLn $ "part2 (x * y): " <> show (x2 * y2)
